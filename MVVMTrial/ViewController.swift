b77e2549be4044a9655206aabb508ab61682da3c
Varun <pvr994@gmail.com>
2024-07-11 11:06:50 +0530

    Initial Commit

---
+++ MVVMTrial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTrial
+//
+//  Created by Raj.P, Varun (Cognizant) on 11/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
